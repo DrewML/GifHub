0c48718eec42b0588a287c540a60db11246d1359
Louis Lagrange <lagrange.louis@gmail.com>
2017-02-01 22:39:05 +0100

    make the extension work with the GitHub API v3

--- src/js/sites/GitHub/api.js
+++ src/js/sites/GitHub/api.js
@@ -1,15 +1,15 @@
-import { getAuthenticityToken, getPreviewUri } from './page';
-
 function getMarkdownPreview(text) {
-    // Upgrade jQuery deferred to an ES6 Promise
-    return Promise.resolve($.ajax({
+    return fetch('https://api.github.com/markdown', {
         method: 'POST',
-        url: getPreviewUri(),
-        data: {
-            authenticity_token: getAuthenticityToken(),
+        headers: {
+            "Content-Type" : "application/json",
+            "Accept": "application/vnd.github.v3+json",
+        },
+        body : JSON.stringify({
+            mode: 'markdown',
             text
-        }
-    }));
+        }),
+    }).then(res => res.text())
 }
 
 export function bypassCSPForImages(images = []) {
@@ -18,14 +18,14 @@ export function bypassCSPForImages(images = []) {
     )).join('\n\n');
 
     return getMarkdownPreview(markdown).then(res => {
-        const $res = $(res);
-        return Array.from($res.filter('p')).map(p => {
-            const imgs = $(p).find('img');
-            return {
-                uri: imgs.get(0).src,
-                name: imgs.get(0).alt,
-                downsizedUri: imgs.get(1).src
-            };
-        });
+      const $res = $(res);
+      return Array.from($res.filter('p')).map(p => {
+        const imgs = $(p).find('img');
+        return {
+          uri: imgs.get(0).src,
+          name: imgs.get(0).alt,
+          downsizedUri: imgs.get(1).src
+        };
+      });
     });
 }
