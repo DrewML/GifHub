9616144c4429453c4e96d5cd61ccb10ff27fa0a8
Louis Lagrange <lagrange.louis@gmail.com>
2017-02-03 20:55:14 +0100

    Fix indentation

--- src/js/sites/GitHub/api.js
+++ src/js/sites/GitHub/api.js
@@ -16,16 +16,16 @@ export function bypassCSPForImages(images = []) {
     const markdown = images.map(image => (
         `![${image.name}](${image.uri}) ![downsized](${image.downsizedUri})`
     )).join('\n\n');
-
+    
     return getMarkdownPreview(markdown).then(res => {
-      const $res = $(res);
-      return Array.from($res.filter('p')).map(p => {
-        const imgs = $(p).find('img');
-        return {
-          uri: imgs.get(0).src,
-          name: imgs.get(0).alt,
-          downsizedUri: imgs.get(1).src
-        };
-      });
+        const $res = $(res);
+        return Array.from($res.filter('p')).map(p => {
+            const imgs = $(p).find('img');
+            return {
+                uri: imgs.get(0).src,
+                name: imgs.get(0).alt,
+                downsizedUri: imgs.get(1).src
+            };
+        });
     });
 }
